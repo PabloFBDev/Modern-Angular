89597c99253cdb165f907031edd2e09d0439f1d3
Pablo Ferreira <pablo.ferreira@solvace.com>
2024-11-20 16:13:00 -0300

    chore: config eslint and prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Modern-Angular';
