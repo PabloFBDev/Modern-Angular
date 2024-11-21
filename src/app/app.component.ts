244f92c1a3cdb98f97f0ca747dd71581518ead00
Pablo Ferreira <pablo.ferreira@solvace.com>
2024-11-20 21:53:57 -0300

    feature: add dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ThemeService } from './services/theme.service';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'Modern-Angular';
+export class AppComponent implements OnInit {
+  private themeService = inject(ThemeService);
+
+  public ngOnInit(): void {
+    const currentColorTheme = this.themeService.getPreferredColorTheme();
+    this.themeService.setColorTheme(currentColorTheme);
+  }
 }
