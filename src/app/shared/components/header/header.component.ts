ec7ccecf4a7a1bf40e596a00fa5231527412e402
Pablo Ferreira <pablo.ferreira@solvace.com>
2024-11-20 22:33:54 -0300

    feat: add css class to center content

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { ThemeToggleComponent } from '../theme-toggle/theme-toggle.component';
 
 @Component({
   selector: 'app-header',
-  imports: [ThemeToggleComponent],
+  imports: [ThemeToggleComponent, MatToolbarModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
--- src/app/shared/components/theme-toggle/theme-toggle.component.ts
+++ src/app/shared/components/theme-toggle/theme-toggle.component.ts
@@ -15,20 +15,18 @@ import { ThemeService } from '../../../services/theme.service';
     MatTooltipModule,
   ],
   template: `
-    <div class="fixed top-0 right-0">
-      <button
-        type="button"
-        mat-icon-button
-        class="m-1 p-0"
-        (click)="themeService.toggleColorTheme()"
-        [matTooltip]="themeService.isCurrentThemeDark() ? 'Claro' : 'Escuro'">
-        @if (themeService.isCurrentThemeDark()) {
-          <mat-icon class="font-icon text-amber-500">light_mode</mat-icon>
-        } @else {
-          <mat-icon class="font-icon text-sky-500">dark_mode</mat-icon>
-        }
-      </button>
-    </div>
+    <button
+      type="button"
+      mat-icon-button
+      class="m-1 p-0"
+      (click)="themeService.toggleColorTheme()"
+      [matTooltip]="themeService.isCurrentThemeDark() ? 'Claro' : 'Escuro'">
+      @if (themeService.isCurrentThemeDark()) {
+        <mat-icon class="font-icon text-amber-500">light_mode</mat-icon>
+      } @else {
+        <mat-icon class="font-icon text-sky-500">dark_mode</mat-icon>
+      }
+    </button>
   `,
   styles: ``,
 })
